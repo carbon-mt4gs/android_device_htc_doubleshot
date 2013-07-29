9160d4900cbbc22e8f7d50544a70c3fe4842c187
Emmanuel Utomi <emmanuelutomi@gmail.com>
2013-07-29 12:07:58 -0500

    Make recovery params are put back in

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,8 +62,12 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 1252770816
 BOARD_FLASH_BLOCK_SIZE := 262144
 
 #Recovery
+TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+TARGET_RECOVERY_UI_LIB := librecovery_ui_doubleshot
+BOARD_HAS_NO_SELECT_BUTTON := false
 TARGET_PROVIDES_INIT := true
 TARGET_RECOVERY_INITRC := device/htc/doubleshot/recovery/init.rc
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/htc/doubleshot/recovery/default_recovery_keys.c
 BOARD_CUSTOM_GRAPHICS := ../../../device/htc/doubleshot/recovery/graphics.c
 BOARD_USES_MMCUTILS := true
+TARGET_RECOVERY_FSTAB = device/htc/doubleshot/ramdisk/fstab.doubleshot
