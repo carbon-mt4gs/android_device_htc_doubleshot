0b637f79b5e532fb8bf11c144547f2c4ac2f9788
lomarb <lxberry@gmail.com>
2013-11-11 21:27:19 -0500

    BoardConfig: Remove BOARD_CUSTOM_GRAPHICS here too.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,5 +76,4 @@ TARGET_RECOVERY_FSTAB := device/htc/doubleshot/ramdisk/fstab.doubleshot
 RECOVERY_FSTAB_VERSION := 2
 TARGET_RECOVERY_INITRC := device/htc/doubleshot/recovery/init.rc
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/htc/doubleshot/recovery/default_recovery_keys.c
-BOARD_CUSTOM_GRAPHICS := ../../../device/htc/doubleshot/recovery/graphics.c
 BOARD_USES_MMCUTILS := true
